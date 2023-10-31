bf7f082a650f989a00d3dea2c9cef129ebb70a24
Tyler Mok <tymok06@gmail.com>
2023-10-31 09:47:40 -0500

    shoppingListApp

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var items = [String]()
+    
 
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
